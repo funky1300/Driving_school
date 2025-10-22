7727defb7ba3183daf4bd4e2f23851fe85fed795
funky1300 <evyatar.aviel2@gmail.com>
2025-10-22 10:11:15 +0300

    not final

--- app/src/main/java/com/example/driving_school/MainActivity.java
+++ app/src/main/java/com/example/driving_school/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.driving_school;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    EditText name, phone_number, email;
+    Button date_bt, time_bt, save_bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,72 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        this.name = findViewById(R.id.name);
+        this.phone_number = findViewById(R.id.phone_number);
+        this.email = findViewById(R.id.email);
+
+        this.date_bt = findViewById(R.id.date_bt);
+        this.time_bt = findViewById(R.id.time_bt);
+        this.save_bt = findViewById(R.id.save_bt);
+
+        date_bt.setOnClickListener(this);
+        time_bt.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v == date_bt)
+        {
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, this, year, month, day);
+            datePickerDialog.show();
+        }
+        if(v == time_bt)
+        {
+            Calendar calendar = Calendar.getInstance();
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, this, hour, minute, true);
+            timePickerDialog.show();
+        }
+
+        if(v == save_bt)
+        {
+            String name_text = name.getText().toString();
+            String phone_number_text = phone_number.getText().toString();
+            String email_text = email.getText().toString();
+            String date_text = date_bt.getText().toString();
+            String time_text = time_bt.getText().toString();
+            driving_lesson lesson = new driving_lesson(name_text, phone_number_text, email_text, date_text, time_text);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(lesson.toString());
+            builder.setTitle("lesson");
+            builder.setPositiveButton("ok", null);
+            builder.setNegativeButton("cancel", null);
+            EditText et = new EditText(this);
+            et.setText(lesson.toString());
+            et.setBackgroundColor(Color.BLUE);
+            builder.setView(et);
+
+            builder.create().show();
+        }
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+        date_bt.setText(date);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        String time = hourOfDay + ":" + minute;
+        time_bt.setText(time);
     }
 }
--- app/src/main/java/com/example/driving_school/driving_lesson.java
+++ app/src/main/java/com/example/driving_school/driving_lesson.java
@@ -4,12 +4,17 @@ public class driving_lesson {
     private String name;
     private String phone_number;
     private String email;
+    private String date;
+    private String time;
 
-    public driving_lesson(String name, String phone_number, String email)
+
+    public driving_lesson(String name, String phone_number, String email, String date, String time)
     {
         this.name = name;
         this.phone_number = phone_number;
         this.email = email;
+        this.date = date;
+        this.time = time;
     }
 
     public String getName()
@@ -40,5 +45,9 @@ public class driving_lesson {
     public void setEmail(String email)
     {
         this.email = email;
+    }
+
+    public String toString() {
+        return "name: " + name + "phone number: " + phone_number + "email: " + email + "date: " + date + "time: " + time;
     }
 }
